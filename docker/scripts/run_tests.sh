8c4b5ada080b523fd70793e5216c2ff2319c3cec
John Marshall <marshall@hep.phy.cam.ac.uk>
2017-11-27 17:54:45 +0000

    Update to reflect recent changes to PandoraSettings file.

--- docker/scripts/run_tests.sh
+++ docker/scripts/run_tests.sh
@@ -1,17 +1,13 @@
 #/bin/bash
-settingsFile=/pandora/LArReco/settings/uboone/PandoraSettings_MicroBooNE.xml
+export FW_SEARCH_PATH=/pandora/LArReco/settings:/pandora/data/:$FW_SEARCH_PATH
+
+settingsFile=/pandora/LArReco/settings/PandoraSettings_Master_MicroBooNE.xml
 cheatSettingsFile=/pandora/LArReco/settings/development/PandoraSettings_Cheat.xml
-geometryFile=/pandora/LArReco/geometry/uboone.xml
+geometryFile=/pandora/LArReco/geometry/PandoraGeometry_MicroBooNE.xml
 
 # Disable monitoring.
 sed -i "s/<IsMonitoringEnabled>.*<\/IsMonitoringEnabled>/<IsMonitoringEnabled>false<\/IsMonitoringEnabled>/g" ${settingsFile}
 sed -i "s/<IsMonitoringEnabled>.*<\/IsMonitoringEnabled>/<IsMonitoringEnabled>false<\/IsMonitoringEnabled>/g" ${cheatSettingsFile}
-
-# Set any SVM files.
-svmFile=/pandora/data/PandoraSvm_MicroBooNE_mcc7.xml
-alteredSvmPath="$(echo $svmFile | sed -e 's/[\/&]/\\&/g')"
-sed -i "s/<SvmFileName>.*<\/SvmFileName>/<SvmFileName>${alteredSvmPath}<\/SvmFileName>/g" ${settingsFile}
-sed -i "s/<SvmFileName>.*<\/SvmFileName>/<SvmFileName>${alteredSvmPath}<\/SvmFileName>/g" ${cheatSettingsFile}
 
 # Run nu samples with neutrino script.
 for f in /pandora/data/samples/nu/*.pndr
