8007fbef5854734cb63b72784c65361a1a1766ad
John Marshall <marshall@hep.phy.cam.ac.uk>
2020-07-14 15:59:14 +0100

    Set ROOT location for lcreco and workshopcontent.

--- docker/scripts/build_workshopcontent.sh
+++ docker/scripts/build_workshopcontent.sh
@@ -6,7 +6,7 @@ source /pandora/set_compiler_flags.sh
 # Build project
 mkdir build
 cd build
-cmake -DCMAKE_MODULE_PATH=/pandora/PandoraPFA/cmakemodules -DPANDORA_MONITORING=ON -DPandoraSDK_DIR=/pandora/PandoraSDK -DPandoraMonitoring_DIR=/pandora/PandoraMonitoring -DLArContent_DIR=/pandora/LArContent ..
+cmake -DCMAKE_MODULE_PATH=/pandora/PandoraPFA/cmakemodules -DROOT_DIR=/pandora/root/cmake -DPANDORA_MONITORING=ON -DPandoraSDK_DIR=/pandora/PandoraSDK -DPandoraMonitoring_DIR=/pandora/PandoraMonitoring -DLArContent_DIR=/pandora/LArContent ..
 
 if [[ "$1" == "coverity" ]]; then
     source /pandora/prepend_coverity_path.sh -fPIC
